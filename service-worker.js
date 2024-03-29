ebfa4f143965641f6dd9911d3888a9f38fd007b5
yevh3ny <koyotische@gmail.com>
2024-03-29 17:25:03 +0200

    2

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('app-cache').then((cache) => {
         return cache.addAll([
-          '/',
+          
           '/index.html',          
         ]);
       })
